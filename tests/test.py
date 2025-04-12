9b91df4ae03f5c9fe108d45701edcac053dacf1c
TuteMthCD <matias.civadda2342001@gmail.com>
2025-04-12 01:37:12 -0300

    test: sync

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,12 @@
 from serial import Serial
-from time import sleep
 
 ser = Serial("/dev/ttyACM0")
 
-buff = bytearray([0x03, 0xA]);
+buff = bytearray([0x10]);
 
 ser.write(buff)
+
+sync = ser.read(2);
+
+if sync == bytearray([0x15, 0x06]):
+    print("sync ok")
